ea193e266c51038bf62fb34f358e76219d8aebfb
lutaoact <lutaoact@hotmail.com>
2016-06-03 15:50:27 +0800

    keys.lua

--- keys.lua
+++ keys.lua
@@ -1,27 +1,11 @@
-local join = function(hash)
-  local str = '';
-  for i, v in pairs(hash) do
-    local tmp = i .. '**' .. v
-    if str == '' then
-      str = tmp
-    end
-    str = str .. '##' .. tmp
-  end
-  return str;
+local result = '';
+local hostCountKeys = redis.call('keys', 'hostCount:'..ARGV[1]..":*")
+for index = 1, #hostCountKeys do
+  local hashKey = hostCountKeys[index]
+  local hash = redis.call('hgetall', hashKey);
+  result = result..hashKey..'##'..table.concat(hash, '**').."\n"
+  redis.log(redis.LOG_NOTICE, "index: "..index..", hashKey: "..hashKey)
+  redis.log(redis.LOG_NOTICE, result)
+--  redis.call('del', hashKey)
 end
-
---local result = {};
---local hostCountKeys = redis.call('keys', 'hostCount'.."*")
---local link;
---for index = 1, #hostCountKeys do
---  local hashKey = hostCountKeys[index]
---  local hash = redis.call('hgetall', hashKey);
---  result[hashKey] = join(hash)
---  redis.log(redis.LOG_NOTICE, "index: "..index..", hashKey: "..hashKey)
---  redis.log(redis.LOG_NOTICE, result[hashKey])
-----  redis.call('del', hashKey)
---end
-local hash = redis.call('hgetall', 'hostCount:20160603:456');
-local str = table.concat(hash, '**');
-redis.log(redis.LOG_NOTICE, str)
-return str;
+return result;
