d4bfa3658f03e1918f4a3acd79560accfc5decce
lutaoact <lutaoact@hotmail.com>
2016-06-20 18:26:48 +0800

    fix_abnormal.lua

---
+++ test.lua
@@ -0,0 +1,10 @@
+print(type(a))   --> nil   (`a' is not initialized)
+a = 10
+print(type(a))   --> number
+a = "a string!!"
+print(type(a))   --> string
+a = print        -- yes, this is valid!
+a(type(a)) 
+
+a = {}
+print(type(a));
