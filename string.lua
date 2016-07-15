d2e388f434ca21e9070a2ae24e292fc2b89ef043
lutaoact <lutaoact@hotmail.com>
2016-07-15 11:20:33 +0800

    table.lua

--- string.lua
+++ string.lua
@@ -1,3 +1,6 @@
 local key = 'dpt:10731077871588:urlCrcs'
 local id = string.sub(key, 5, 18)
 print(id)
+
+--string.sub (s, i [, j])
+--Returns the substring of s that starts at i and continues until j; i and j can be negative. If j is absent, then it is assumed to be equal to -1 (which is the same as the string length). In particular, the call string.sub(s,1,j) returns a prefix of s with length j, and string.sub(s, -i) returns a suffix of s with length i.
---
+++ table.lua
@@ -0,0 +1,6 @@
+t = {a=10}
+if next(t) == nil then
+  print('empty')
+else
+  print('not empty')
+end
