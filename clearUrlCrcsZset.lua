a848d743efe9e9e97e323ef016e7dd45765ae0eb
lutaoact <lutaoact@hotmail.com>
2016-07-18 17:00:15 +0800

    clearUrlCrcsZset.lua: completed

--- clearUrlCrcsZset.lua
+++ clearUrlCrcsZset.lua
@@ -1,15 +1,37 @@
---local timestamp = ARGV[1] --取出服务器时间的秒时间戳
+redis.replicate_commands() --3.2版本之后才新增的东西
 
-local cursor = 0
+local timestamp = tonumber(redis.call('time')[1]) - 7 * 86400
+redis.log(redis.LOG_NOTICE, 'timestamp:', timestamp)
+
+local function clearOne(key)
+  local hylandaId = string.sub(key, 5, 18)
+  redis.log(redis.LOG_NOTICE, hylandaId)
+  local urlCrcs = redis.call('zrangebyscore', key, '-inf', timestamp)
+  local values = redis.call(
+    'hmget', 'dpt:'..hylandaId..':goOnUpdateCount', unpack(urlCrcs)
+  )
+  local reducedCountChange = 0
+  for i = 1, #values do
+    if values[i] then
+      reducedCountChange = reducedCountChange + tonumber(values[i])
+    end
+  end
+  redis.log(redis.LOG_NOTICE, reducedCountChange)
+  redis.call('hincrby', 'dpt:reducedCount', hylandaId, reducedCountChange)
+  redis.call('hdel', 'dpt:'..hylandaId..':goOnUpdateCount', unpack(urlCrcs))
+  redis.call('zremrangebyscore', key, '-inf', timestamp)
+end
+
+local cursor = '0'
 repeat
   local result = redis.call(
     'scan', cursor, 'match', 'dpt:*:urlCrcs', 'count', 100
   )
-  cursor = tonumber(result[1])
-  redis.log(redis.LOG_NOTICE, type(cursor))
+  cursor = result[1]
   local urlCrcsKeys = result[2]
   for i = 1, #urlCrcsKeys do
     local key = urlCrcsKeys[i]
-    redis.log(redis.LOG_NOTICE, key, type(key))
+    redis.log(redis.LOG_NOTICE, key)
+    clearOne(key)
   end
-until (cursor == 0)
+until (cursor == '0')
--- test.lua
+++ test.lua
@@ -1,10 +1,27 @@
-print(type(a))   --> nil   (`a' is not initialized)
-a = 10
-print(type(a))   --> number
-a = "a string!!"
-print(type(a))   --> string
-a = print        -- yes, this is valid!
-a(type(a)) 
 
-a = {}
-print(type(a));
+function clearOne(hylandaId)
+  print(hylandaId)
+end
+
+local totalChange = 0
+
+totalChange += (false or 1)
+print(totalChange)
+
+--clearOne('xxxxxxxxxxxx')
+
+
+--local sha1 = require 'sha1'
+--local hash_as_hex = sha1('xxx')
+--print(hash_as_hex)
+
+--print(type(a))   --> nil   (`a' is not initialized)
+--a = 10
+--print(type(a))   --> number
+--a = "a string!!"
+--print(type(a))   --> string
+--a = print        -- yes, this is valid!
+--a(type(a)) 
+--
+--a = {}
+--print(type(a));
