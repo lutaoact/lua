585c9d3f1d791ae6ec5a450d1ea27ccf1fb82d83
lutaoact <lutaoact@hotmail.com>
2017-03-11 11:39:59 +0800

    zinterstoreMemberStocksEffectRank.sh: add crontab conf

--- zinterstoreMemberStocksEffectRank.sh
+++ zinterstoreMemberStocksEffectRank.sh
@@ -1,1 +1,3 @@
 \redis-cli -h mongo -n 1 --eval zinterstoreMemberStocksEffectRank.lua , $(date +'%Y%m%d')
+# crontab中的配置行参考
+# 0 8 * * * . ~/.bash_profile && cd ~/lua && PATH=/usr/local/bin:$PATH \redis-cli -h mongo -n 1 --eval zinterstoreMemberStocksEffectRank.lua >> /data/log/zinterstoreMemberStocksEffectRank.log 2>&1
