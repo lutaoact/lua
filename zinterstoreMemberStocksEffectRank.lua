7790e7a1bbee312748dc61091c728c5a7ab1744a
lutaoact <lutaoact@hotmail.com>
2017-03-11 11:27:08 +0800

    zinterstoreMemberStocksEffectRank.lua

---
+++ zinterstoreMemberStocksEffectRank.lua
@@ -0,0 +1,13 @@
+local date = ARGV[1]
+
+local mKeys = redis.call('keys', 'm:*')
+for i = 1, #mKeys do
+  local mKey = mKeys[i]
+  -- 生成的key格式为erm:code
+  local dstKey = 'er'..mKey
+  redis.call('zinterstore', dstKey, 2, mKey, 'er2:'..date, 'weights', 0, 1)
+  redis.log(redis.LOG_NOTICE, mKey, dstKey)
+end
+
+local now = redis.call('time')[1]
+return 'finished '..now
