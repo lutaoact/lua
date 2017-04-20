eb127637f0c35b7af00a5233c1fff002f0540bf6
lutaoact <lutaoact@hotmail.com>
2017-04-20 15:30:40 +0800

    buildJiaoYiLiangStats.lua

---
+++ buildJiaoYiLiangStats.lua
@@ -0,0 +1,42 @@
+-- \redis-cli -h mongo -n 6 --eval buildJiaoYiLiangStats.lua codeFirstTime tb:600000 jyls:600000 , 600000 1492669773 3.22 50
+-- \redis-cli -h mongo -n 6 --eval buildJiaoYiLiangStats.lua codeFirstTime tb:600000 jyls:600000 , 600000 1492669779 3.23 50
+
+-- 数据类型 codeFirstTime => hash, timeBits => bit, stats => zset
+local codeFirstTime, timeBits, stats = unpack(KEYS)
+local code, curTime, price, quantity = unpack(ARGV)
+
+local firstTime = redis.call('hget', codeFirstTime, code)
+
+local function saveFirst()
+  redis.call('hset', codeFirstTime, code, curTime)
+  redis.call('setbit', timeBits, 0, 1)
+  redis.call('zadd', stats, quantity, price)
+end
+
+if firstTime then
+  redis.log(redis.LOG_NOTICE, 'firstTime:', firstTime)
+  firstTime = tonumber(firstTime)
+  curTime   = tonumber(curTime)
+  local diff = curTime - firstTime
+  redis.log(redis.LOG_NOTICE, 'diff:', diff)
+  if diff > 6 * 3600 then
+    redis.log(redis.LOG_NOTICE, 'another day')
+    redis.call('del', timeBits, stats)
+
+    saveFirst()
+  else
+    redis.log(redis.LOG_NOTICE, 'same day')
+    local bit = redis.call('setbit', timeBits, diff, 1)
+    redis.log(redis.LOG_NOTICE, 'bit:', bit)
+    redis.log(redis.LOG_NOTICE, 'bit:', type(bit))
+    if bit == 0 then
+      redis.log(redis.LOG_NOTICE, 'in bit == 0')
+      redis.call('zincrby', stats, quantity, price)
+    end
+  end
+else
+  redis.log(redis.LOG_NOTICE, type(firstTime), 'no firstTime')
+  saveFirst()
+end
+
+return {curTime, price, quantity}
