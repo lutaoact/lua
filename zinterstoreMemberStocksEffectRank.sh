813f9a59d7779c314a447180b44dfeefdb911567
lutaoact <lutaoact@hotmail.com>
2017-06-20 10:12:49 +0800

    zinterstoreMemberStocksEffectRank.lua: fix erm:*

--- zinterstoreMemberStocksEffectRank.sh
+++ zinterstoreMemberStocksEffectRank.sh
@@ -1,3 +1,3 @@
-\redis-cli -h mongo -n 1 --eval zinterstoreMemberStocksEffectRank.lua , $(date +'%Y%m%d')
+\redis-cli -h mongo -n 1 --eval zinterstoreMemberStocksEffectRank.lua
 # crontab中的配置行参考
 # */10 * * * * . ~/.bash_profile && cd ~/lua && PATH=/usr/local/bin:$PATH sh zinterstoreMemberStocksEffectRank.sh >> /data/log/zinterstoreMemberStocksEffectRank.log 2>&1
